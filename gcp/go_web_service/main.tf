c9717cb2b6ccabdcdd9a37f1921afc46cc683a6b
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2026-02-22 14:28:03 +0100

    Fix circular reference

--- gcp/go_web_service/main.tf
+++ gcp/go_web_service/main.tf
@@ -126,7 +126,7 @@ resource "google_compute_region_network_endpoint_group" "network_endpoint_group"
     network_endpoint_type = "SERVERLESS"
 
     cloud_run {
-        service = google_cloud_run_v2_service.service.name
+        service = var.name
     }
 }
 
